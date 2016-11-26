2f09eb8c87e5fe341032e89dcbb9eda650cfedd6
Avi Goldman <avrahamymgoldman@gmail.com>
2016-11-26 14:28:51 -0500

    first pass at a conversation

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 const ngrok = require('ngrok');
-var Sparky = require('./lib/bot.js');
+var SparkyBot = require('./lib/bot.js');
 
 var port = process.env.PORT || 3000;
 
 ngrok.connect(port, function (err, url) {
-
-  var sparky = Sparky({
+  // create the email bot
+  var sparky = SparkyBot({
     email_key: '056cf4213c89156180708f57e3a38bed59451563',
     auth_token: 'my_secret_key',
     bot_name: 'sparky',
@@ -15,34 +15,49 @@ ngrok.connect(port, function (err, url) {
     inbound_address: 'robot@sendmailfor.me',
     domain: url,
     restrict_inbound: false,
-    setup: true
+    setup: true,
   });
 
-  sparky.on('email_received', function(bot, message) {
+  sparky.hears('hello', 'email_received', function(bot, message) {
     bot.reply(message, {
-      'body': 'Hello back to you'
+      'body': 'Hello back to you'+JSON.stringify(message.matches, null, 2)
     });
   });
 
+  sparky.on('email_received', function(bot, message) {
 
+    bot.startConversation(message, function(err, convo) {
+
+      convo.ask('What\'s your name?', function(convo, response) {
+        
+        convo.say('Hi '+sparky.clean(reponse.text));
+        convo.next();
+      });
+
+    }); 
+
+  });
+
+
+  // start the server
   sparky.setupServer(port, function(err, server) {
     sparky.setupEndpoint(server, function() {
-      // sparky.processMessage({
-      //   "content": {
-      //     "headers": [],
-      //     "html": "<p>Hi there <strong>SparkPostians</strong>.</p>",
-      //     "subject": "We come in peace",
-      //     "text": "Hi there SparkPostians.",
-      //     "to": [
-      //       "robot@sendmailfor.me"
-      //     ],
-      //     "cc": [
-      //       "jhacks.umd@gmail.com"
-      //     ]
-      //   },
-      //   "msg_from": "avigoldmankid@gmail.com",
-      //   "rcpt_to": "robot@sendmailfor.me",
-      // });
+      sparky.processMessage({
+        "content": {
+          "headers": [],
+          "html": "<p>Hello there <strong>SparkPostians</strong>.</p>",
+          "subject": "We come in peace",
+          "text": "Hi there SparkPostians.",
+          "to": [
+            "robot@sendmailfor.me"
+          ],
+          "cc": [
+            "jhacks.umd@gmail.com"
+          ]
+        },
+        "msg_from": "avigoldmankid@gmail.com",
+        "rcpt_to": "robot@sendmailfor.me",
+      });
     });
   });
 });
